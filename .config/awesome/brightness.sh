f04b518021299eae9ee6d7abfee72236a4773187
micro-hawk <dasvikas0071@gmail.com>
2023-05-07 17:55:54 +0530

    brightness edited

--- .config/awesome/brightness.sh
+++ .config/awesome/brightness.sh
@@ -9,7 +9,7 @@ read brightness_percentage
 brightness=$(($brightness_percentage*$max_brightness/100));
 # echo $brightness
 if [ $brightness -le $max_brightness ]; then
-    echo "$brightness" > /sys/class/backlight/intel_backlight/brightness
+    echo "$brightness" > /sys/class/backlight/${dir_contents}/brightness
 else
     echo "Invalid brightness %"
 fi
